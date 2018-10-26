32c4ba25ada7f15c286de2ff876c8403b161fe24
SzilvasiPeter <peti.szilvasi95@gmail.com>
2018-10-26 17:44:52 +0200

    Try out conditions and loops

--- Classes&Methods/C#_Practice.cs
+++ Classes&Methods/C#_Practice.cs
@@ -1,35 +1,72 @@
-class OurProgram
+using System;
+
+namespace Practice
 {
-    static void Main()
+    class OurProgram
     {
-        // Calculate();
-        // CalcUserInput();
-        System.Console.Write("Please enter a number to be added to 5");
-        string userInput = System.Console.ReadLine();
-        CalcUserInputParams(userInput);
-    }
+        static void Main()
+        {
+            // Calculate();
+            CalcUserInput();
+            // Console.Write("Please enter a number to be added to 5");
+            // string userInput = Console.ReadLine();
+            // CalcUserInputParams(userInput);
+        }
 
-    // static void Calculate(){
-    //     int valueOne = 2;
-    //     int valueTwo = 3;
-    //     int result;
+        // static void Calculate(){
+        //     int valueOne = 2;
+        //     int valueTwo = 3;
+        //     int result;
 
-    //     result = valueOne + valueTwo;
-    //     System.Console.WriteLine(result);;
-    // }
+        //     result = valueOne + valueTwo;
+        //     Console.WriteLine(result);;
+        // }
 
-    // static void CalcUserInput(){
-    //     System.Console.WriteLine("Please enter a number to be added ");
-    //     int valueFive = 5;
-    //     int userInput = System.Convert.ToInt32(System.Console.ReadLine());
-    //     int result = valueFive + userInput;
-    //     System.Console.WriteLine("That number plus 5 is " + result);
-    // }
+        static void CalcUserInput(){
+            Console.WriteLine("Please enter a number between 1 and 10");
+            int userInput= Convert.ToInt32(Console.ReadLine());
+            // IF/ELSE
+            bool outOfRange = false;
+            if(userInput > 1 && userInput < 5){
+                Console.Write("That number is between 1 and 5");
+            }
+            else if(userInput > 5 && userInput < 10){
+                Console.Write("That number is between 5 and 10");
+            }
+            else if (userInput < 1 || userInput > 10){
+                outOfRange = true;
+                if(outOfRange == true){
+                    Console.Write("That number is out of range");
+                }
+            }
+            else{
+                Console.Write("That number is 1, 5, or 10");
+            }
 
-    static void CalcUserInputParams(string userParams){
-        int valueFive = 5;
-        int userInput = System.Convert.ToInt32(userParams);
-        int result = valueFive + userInput;
-        System.Console.WriteLine("That number plus 5 is " + result);
+            // //WHILE loops
+            // while(userInput < 10){
+            //     userInput += 1;
+            //     Console.Write("\n" + userInput + "\n");
+            //     if(userInput > 10 || userInput < 1){
+            //         break;
+            //     }
+            // }
+            Console.Write("Process Done");
+            //FOR loop
+            for(int i = userInput; i <= 10; i++){
+                Console.Write("\n" + i + "\n");
+                if(userInput > 10 || userInput < 1){
+                    break;
+                }
+            }
+            Console.Write("Process Done");
+        }
+
+        // static void CalcUserInputParams(string userParams){
+        //     int valueFive = 5;
+        //     int userInput = Convert.ToInt32(userParams);
+        //     int result = valueFive + userInput;
+        //     Console.WriteLine("That number plus 5 is " + result);
+        // }
     }
 }
