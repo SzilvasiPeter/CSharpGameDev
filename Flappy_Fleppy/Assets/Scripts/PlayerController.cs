80a5ed33117a3eb707915bdae49070dd76f132d8
SzilvasiPeter <peti.szilvasi95@gmail.com>
2018-11-11 16:03:24 +0100

    Add comment for mobile input, If you want play download unity remote 5 into you mobile

--- Flappy_Fleppy/Assets/Scripts/PlayerController.cs
+++ Flappy_Fleppy/Assets/Scripts/PlayerController.cs
@@ -30,10 +30,16 @@ public class PlayerController : MonoBehaviour {
 
     void PlayerInput()
     {
+        // Input for PC
         if(Input.GetKey(KeyCode.Space))
         {
             rBody.AddForce(Vector2.up * 50);
         }
+        // original namespace: com.Company.ProductName
+        // Input for mobile phone (android)
+        /*if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved){
+            rBody.AddForce(Vector2.up * 50);
+        }*/
     }
 
     void PlayerMovoment(){
