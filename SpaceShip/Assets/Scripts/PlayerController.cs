05628b5e1bff81040028c3c46a8507f7760e44db
SzilvasiPeter <peti.szilvasi95@gmail.com>
2018-11-20 15:05:24 +0100

    Create the Scene with Sun and Planets

---
+++ SpaceShip/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody2D rbody;
+
+    [SerializeField]
+    private ParticleSystem flares;
+
+	// Use this for initialization
+	void Start () {
+        rbody = this.gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        bool goForward = Input.GetKey(KeyCode.W);
+        bool turnLeft = Input.GetKey(KeyCode.A);
+        bool turnRight = Input.GetKey(KeyCode.D);
+
+        if (goForward)
+        {
+            rbody.AddForce(this.transform.up * -2);
+        } else if (turnLeft)
+        {
+            rbody.AddTorque(500);
+        } else if (turnRight)
+        {
+            rbody.AddTorque(-500);
+        }
+    }
+}
