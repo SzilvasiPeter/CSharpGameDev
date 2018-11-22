2bc5fbc1dccf681be3fe1470cecf740dca4662f3
SzilvasiPeter <peti.szilvasi95@gmail.com>
2018-11-22 12:36:05 +0100

    Adding smooth camera follower and spaceship can shot laser

--- SpaceShip/Assets/Scripts/PlayerController.cs
+++ SpaceShip/Assets/Scripts/PlayerController.cs
@@ -6,8 +6,11 @@ public class PlayerController : MonoBehaviour {
 
     private Rigidbody2D rbody;
 
+    //[SerializeField]
+    //private ParticleSystem flares;
+
     [SerializeField]
-    private ParticleSystem flares;
+    private GameObject fireball;
 
 	// Use this for initialization
 	void Start () {
@@ -16,19 +19,33 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        fireLaser();
         bool goForward = Input.GetKey(KeyCode.W);
         bool turnLeft = Input.GetKey(KeyCode.A);
         bool turnRight = Input.GetKey(KeyCode.D);
 
         if (goForward)
         {
-            rbody.AddForce(this.transform.up * -2);
+            rbody.AddForce(this.transform.up * -1.5f);
         } else if (turnLeft)
         {
             rbody.AddTorque(500);
         } else if (turnRight)
         {
             rbody.AddTorque(-500);
+        }
+    }
+
+    void fireLaser()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Transform spawnArea = gameObject.transform.GetChild(0).transform;
+            GameObject laserClone = Instantiate(fireball, spawnArea.position, spawnArea.rotation);
+
+            laserClone.GetComponent<Rigidbody2D>().AddForce(this.transform.up * -700);
+
+            Destroy(laserClone, 2.0f);
         }
     }
 }
---
+++ SpaceShip/Assets/Scripts/SmoothCameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothCameraFollow : MonoBehaviour {
+
+    private Transform playerTrans;
+    private float smoothTime = 0.5f;
+    private Vector3 velocity = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+        playerTrans = GameObject.Find("Player").transform;		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 targetPosition = new Vector3(playerTrans.position.x, playerTrans.position.y, transform.position.z);
+        transform.position = Vector3.SmoothDamp( transform.position, targetPosition, ref velocity, smoothTime);
+	}
+}
