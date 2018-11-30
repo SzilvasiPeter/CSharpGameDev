a7b9ee95fdac66604001dcc8224497219ed52825
SzilvasiPeter <peti.szilvasi95@gmail.com>
2018-11-30 11:03:00 +0100

    Apply gravity (the circular movement) to all Planets

--- SpaceShip/Assets/Scripts/CircularMovement.cs
+++ SpaceShip/Assets/Scripts/CircularMovement.cs
@@ -4,17 +4,27 @@ using UnityEngine;
 
 public class CircularMovement : MonoBehaviour {
 
-    private float rotateSpeed = 0.5f;
-    private float radius = -30.0f;
+    //private float rotateSpeed = 0.3f;
+    //private float radius = -30.0f;
+    private float radius;
+    private float rotateSpeed;
 
-    private Vector3 sun;
+    private Transform sun;
     private Vector3 center;
     private float angle;
 
 	// Use this for initialization
 	void Start () {
-        sun = GameObject.Find("Sun").transform.position;
-        center = sun;
+        sun = GameObject.Find("Sun").transform;
+        center = sun.position;
+
+        // Distance between Planets and Sun (times -1 for put the planets to the another side)
+        radius = Vector3.Distance(sun.position, this.transform.position) * -1;
+
+        // This 50 just some magic number, which looks okay
+        // Distance is exponentially slow down the rotation
+        rotateSpeed = 50 / (radius * radius);
+        print(rotateSpeed);
 	}
 	
 	// Update is called once per frame
