83c03c24a7ab4ee4f87062844060e7c3622faab2
SzilvasiPeter <peti.szilvasi95@gmail.com>
2018-12-08 16:06:20 +0100

    Add a PlayerController script to the Player obeject, Add player movoment and character controller for colliding with the other objects

--- Shooter/Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Shooter/Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    private float speed = 6.0f;
+    private float gravity = 20.0f;
+    private float rotSpeed = 90.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
 	
 	// Update is called once per frame
 	void Update () {
-		
+        PlayerMovoment();
 	}
+
+    void PlayerMovoment()
+    {
+        transform.Rotate(0, Input.GetAxis("Horizontal") * rotSpeed * Time.deltaTime, 0);
+
+        CharacterController controller = gameObject.GetComponent<CharacterController>();
+        if (controller.isGrounded)
+        {
+            moveDirection = Vector3.forward * Input.GetAxis("Vertical");
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
 }
