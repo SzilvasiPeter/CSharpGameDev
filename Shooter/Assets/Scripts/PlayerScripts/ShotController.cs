16497103943bf4425868dd82853fc39ff18402eb
SzilvasiPeter <peti.szilvasi95@gmail.com>
2018-12-08 16:41:14 +0100

    Add shooting ability to player, and bullet prefab, which I instantiate

---
+++ Shooter/Assets/Scripts/PlayerScripts/ShotController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotController : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject bullet;
+
+    [SerializeField]
+    private Transform spawnAre;
+
+    public int timer = 0;    
+
+	// Use this for initialization
+	void Update () {
+        timer += 1;
+        SpawnBullet();
+	}
+
+    void SpawnBullet()
+    {
+        if(Input.GetMouseButton(0) && timer >= 20)
+        {
+            GameObject bulletClone = Instantiate(bullet, spawnAre.position, spawnAre.rotation);
+
+            bulletClone.GetComponent<Rigidbody>().AddForce(spawnAre.transform.up * -200);
+            timer = 0;
+            Destroy(bulletClone, 1.0f);
+        }
+    }
+}
