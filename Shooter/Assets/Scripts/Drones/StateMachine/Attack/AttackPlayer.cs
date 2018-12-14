67c60cc677c630b7397c9cda078227df08e07beb
SzilvasiPeter <peti.szilvasi95@gmail.com>
2018-12-14 21:30:17 +0100

    Drones will shot after the player in there attacking sphere

---
+++ Shooter/Assets/Scripts/Drones/StateMachine/Attack/AttackPlayer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AttackPlayer : MonoBehaviour {
+
+    public float radius = 0f;
+    public Collider[] colliderSearch;
+
+    public bool playerDetected = false;
+
+    [SerializeField]
+    private GameObject laser;
+
+    public int timer = 0;
+    public Transform laserSpawn;
+	
+	// Update is called once per frame
+	void Update () {
+        timer += 1;
+
+        playerDetected = false;
+        colliderSearch = Physics.OverlapSphere(transform.position, radius);
+
+        foreach(Collider collider in colliderSearch)
+        {
+            if(collider.tag == "player")
+            {
+                print("Player found");
+                Transform playerPos = GameObject.Find("Player").transform;
+                transform.LookAt(playerPos.position);
+
+                if(timer >= 15)
+                {
+                    GameObject laserClone = Instantiate(laser, laserSpawn.transform.position, laser.transform.rotation);
+                    laserClone.transform.rotation = Quaternion.Euler(100.81f, 57.34f, -56.90f);
+                    laserClone.GetComponent<Rigidbody>().AddForce(laserSpawn.transform.forward * 500);
+                    timer = 0;
+                    Destroy(laserClone, 0.5f);
+                }
+            }
+        }
+
+    }
+}
---
+++ Shooter/Assets/Scripts/Drones/StateMachine/Patrol/NavAgent.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavAgent : MonoBehaviour {
+
+    [SerializeField]
+    private Transform[] wayPoints;
+    private int destPoint = 0;
+    NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+
+        agent.autoBraking = false;
+
+        GoToNextPoint();
+	}
+
+    void GoToNextPoint()
+    {
+        if(wayPoints.Length == 0)
+        {
+            return;
+        }
+        agent.destination = wayPoints[destPoint].position;
+
+        destPoint = (destPoint + 1) % wayPoints.Length;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+        {
+            GoToNextPoint();
+        }
+	}
+}
