805dae9c3aa2ba687f979b69c720e5d180364b1e
SzilvasiPeter <peti.szilvasi95@gmail.com>
2018-12-15 13:41:02 +0100

    Add collision to drone and bullet, the dronehealth decrease when bullet hit the drone object, after dronhealth fillamount is 0 the drones are destroyed

---
+++ Shooter/Assets/Scripts/Drones/PatrollingHealth/PatrollingDroneHealth.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PatrollingDroneHealth : MonoBehaviour {
+
+    public Image enemyHealth;
+    //public GameObject explosion;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemyHealth.fillAmount <= 0)
+        {
+            //GameObject explosionClone = Instantiate(explosion, gameObject.transform.position, gameObject.transform.rotation);
+            //Destroy(explosion, 0.5f);
+            Destroy(gameObject, 0.5f);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "bullet")
+        {
+            enemyHealth.fillAmount -= 0.09f;
+        }
+    }
+}
---
+++ Shooter/Assets/Scripts/Drones/StationaryHealth/DroneStationaryHealth.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DroneStationaryHealth : MonoBehaviour {
+
+    public Image enemyHealth;
+    //public GameObject explosion;
+	
+	// Update is called once per frame
+	void Update () {
+		if(enemyHealth.fillAmount <= 0)
+        {
+            //GameObject explosionClone = Instantiate(explosion, gameObject.transform.position, gameObject.transform.rotation);
+            //Destroy(explosion, 0.5f);
+            Destroy(gameObject, 0.5f);
+        }
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "bullet")
+        {
+            enemyHealth.fillAmount -= 0.09f;
+        }
+    }
+}
