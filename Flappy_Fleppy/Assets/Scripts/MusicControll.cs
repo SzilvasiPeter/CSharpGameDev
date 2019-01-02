948e937ebc49686aed13eabc0b9134aebf821bbd
SzilvasiPeter <peti.szilvasi95@gmail.com>
2019-01-02 10:13:11 +0100

    Add softer music to the game, When player died then music transition to quiter state and playing a drump sting

---
+++ Flappy_Fleppy/Assets/Scripts/MusicControll.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MusicControll : MonoBehaviour {
+
+    public AudioMixerSnapshot dead;
+    public AudioClip[] stings;
+    public AudioSource stingSource;
+    public float bpm = 128;
+
+    private float m_TransitionIn;
+    private float m_QuarterNote;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_QuarterNote = 60 / bpm;
+        m_TransitionIn = m_QuarterNote;
+
+    }
+
+     void OnCollisionEnter2D(Collision2D collision)
+    {
+    	if(collision.gameObject.tag == "Danger")
+        {
+            dead.TransitionTo(m_TransitionIn);
+            PlaySting();
+        }
+    }
+
+    void PlaySting()
+    {
+        int randClip = Random.Range(0, stings.Length);
+        stingSource.clip = stings[randClip];
+        stingSource.Play();
+    }
+
+}
